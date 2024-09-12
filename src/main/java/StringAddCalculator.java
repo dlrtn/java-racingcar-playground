a80412ee2eef98f03a110ef0b4154a34b4d3f4a5
JooIcksoo <dlrtn751@gmail.com>
2024-09-12 15:56:53 +0900

    feat: splitAndSum_null_또는_빈문자 green

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,6 +1,9 @@
 public class StringAddCalculator {
 
     public static int splitAndSum(String string) {
+        if (string == null || string.equals("")) {
+            return 0;
+        }
         return -1;
     }
 
