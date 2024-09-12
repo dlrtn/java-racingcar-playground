a84b16e1ef12c488eb302595e0b93564d21702ad
JooIcksoo <dlrtn751@gmail.com>
2024-09-12 15:57:53 +0900

    feat: splitAndSum_null_또는_빈문자 refactor

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,7 +1,7 @@
 public class StringAddCalculator {
 
     public static int splitAndSum(String string) {
-        if (string == null || string.equals("")) {
+        if (string == null || string.isEmpty()) {
             return 0;
         }
         return -1;
