1f212d5fa55efed6d1f134956a57e9765b79b78c
JooIcksoo <dlrtn751@gmail.com>
2024-09-12 16:08:53 +0900

    feat: splitAndSum_쉼표구분자 blue

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,19 +1,35 @@
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String string) {
-        if (string == null || string.isEmpty()) {
+        if (isValidString(string)) {
             return 0;
         }
 
+        int[] numbers = splitAndConvertToIntArray(string);
+
+        return sum(numbers);
+    }
+
+    private static int sum(int[] numbers) {
         int sum = 0;
 
-        String[] strings = string.split(",");
-
-        for (String s : strings) {
-            sum += Integer.parseInt(s);
+        for (int number : numbers) {
+            sum += number;
         }
 
         return sum;
+    }
+
+    private static int[] splitAndConvertToIntArray(String string) {
+        return Arrays.stream(string.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static boolean isValidString(String string) {
+        return string == null || string.isEmpty();
     }
 
 }
